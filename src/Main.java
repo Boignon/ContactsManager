5a73a15f93e433beccd77b8a1101e8a2a9c6a54d
B2AC <boignonangechristian@gmail.com>
2026-05-20 16:25:28 +0000

    création contact

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String [] args){
         ContactsManager myContactsManager = new
                 ContactsManager();
+        Contact myFriends = new Contact();
     }
 }
 class ContactsManager {
     Contact[] myFriends;
     int friendsCount;
+}
+class Contact {
+    String name;
+    String phoneNumber;
 }
