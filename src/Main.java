cae808ae1d92ebaeb856d44e62f66c91245d47c3
B2AC <boignonangechristian@gmail.com>
2026-05-20 17:22:56 +0000

    Modicatication des erreures

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,33 @@ public class Main {
     public static void main(String [] args){
         ContactsManager myContactsManager = new
                 ContactsManager();
-        Contact myFriends = new Contact();
+        //Ajout des contacts
+        Contact myFriends1 = new Contact();
+        myFriends1.name = "ourielle";
+        myFriends1.phoneNumber = "0512345678";
+        myContactsManager.addContact(myFriends1);
 
-        myFriends.name = "ourielle";
-        myFriends.phoneNumber = "0512345678";
-        myContactsManager.addContact(myFriends);
+        Contact myFriends2 = new Contact();
+        myFriends2.name = "Andy";
+        myFriends2.phoneNumber = "0588754216";
+        myContactsManager.addContact(myFriends2);
 
-        myFriends.name = "Andy";
-        myFriends.phoneNumber = "0588754216";
-        myContactsManager.addContact(myFriends);
+        Contact myFriends3 = new Contact();
+        myFriends3.name = "Christiane";
+        myFriends3.phoneNumber = "0523369541";
+        myContactsManager.addContact(myFriends3);
 
-        myFriends.name = "Christiane";
-        myFriends.phoneNumber = "0523369541";
-        myContactsManager.addContact(myFriends);
+        Contact myFriends4 = new Contact();
+        myFriends4.name = "loisse";
+        myFriends4.phoneNumber = "0124587956";
+        myContactsManager.addContact(myFriends4);
 
-        myFriends.name = "loisse";
-        myFriends.phoneNumber = "0124587956";
-        myContactsManager.addContact(myFriends);
+        Contact myFriends5 = new Contact();
+        myFriends5.name = "esteban";
+        myFriends5.phoneNumber = "0745852365";
+        myContactsManager.addContact(myFriends5);
 
-        myFriends.name = "esteban";
-        myFriends.phoneNumber = "0745852365";
-        myContactsManager.addContact(myFriends);
-
+        // Affichage
         Contact result = myContactsManager.searchContact("Andy");
         if (result != null) {
             System.out.println("Numéro de Andy : " + result.phoneNumber);
@@ -34,14 +39,19 @@ class ContactsManager {
     Contact[] myFriends;
     int friendsCount;
 
+    // CONSTRUCTEUR : initialise le tableau
     ContactsManager(){
         this.friendsCount = 0;
         this.myFriends = new Contact[500];
     }
 
+    //Ajoute un nouveau contact au tableau.
     void addContact(Contact contact){
         myFriends[friendsCount] = contact;
         friendsCount++; }
+
+    /*Recherche un contact par nom et
+    retourne l'objet Contact correspondant.*/
     Contact searchContact(String searchName){
         for(int i=0; i<friendsCount; i++){
             if(myFriends[i].name.equals(searchName)){
