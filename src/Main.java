82ba395834edd014a5a2143fcdaff6dcbd1d10b4
B2AC <boignonangechristian@gmail.com>
2026-05-20 16:50:46 +0000

    Appel la méthode addContact

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,23 @@ public class Main {
 
         myFriends.name = "ourielle";
         myFriends.phoneNumber = "0512345678";
+        myContactsManager.addContact(myFriends);
 
     }
 }
 class ContactsManager {
     Contact[] myFriends;
     int friendsCount;
+
+    ContactsManager(){
+        this.friendsCount = 0;
+        this.myFriends = new Contact[500];
+    }
+
+    void addContact(Contact contact){
+        myFriends[friendsCount] = contact;
+        friendsCount++; }
+
 }
 class Contact {
     String name;
