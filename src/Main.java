ed1bf7e0d1172d6bcc09c850022cfbb0730587e7
B2AC <boignonangechristian@gmail.com>
2026-05-20 17:00:53 +0000

    Recherche des Contats

--- src/Main.java
+++ src/Main.java
@@ -38,6 +38,13 @@ class ContactsManager {
     void addContact(Contact contact){
         myFriends[friendsCount] = contact;
         friendsCount++; }
+    Contact searchContact(String searchName){
+        for(int i=0; i<friendsCount; i++){
+            if(myFriends[i].name.equals(searchName)){
+                return myFriends[i];
+            }
+        }
+        return null;}
 
 }
 class Contact {
